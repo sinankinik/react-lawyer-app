08b8f8c0746f8e21308d455e3345adcf3ef413bb
Sinan Kinik <sinankinik@gmail.com>
2023-11-17 15:01:26 +0300

    Initialize Project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#1e2738',
+        'blue': '#3593d1',
+        'orange': '#ef9f02',
+        'bgwhite': '#f5f5f5',
+        'secondwhite': '#fdfdfd',
+      },
+    },
+  },
+  plugins: [],
+}
