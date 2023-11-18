a0d4a971a05c033e9ddde486a4c5b78b9a7d0f16
Sinan Kinik <sinankinik@gmail.com>
2023-11-18 09:56:26 +0300

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Homepage />} />
-    </Routes>
+    <Homepage />
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import Layout from './layout/Layout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Layout>
-            <App />
-        </Layout>
-    </BrowserRouter>
+    <Layout>
+        <App />
+    </Layout>
 )
